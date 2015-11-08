4754560dcae4716f3d198d756be08cc44cbd650b
yas99en <yas99en@gmail.com>
2015-11-08 21:31:53 +0900

    Added clipboard_edit.png and other 2 files. Modified ClipShare.java and Tray.java.

--- clipshare/src/main/java/io/github/yas99en/clipshare/ClipShare.java
+++ clipshare/src/main/java/io/github/yas99en/clipshare/ClipShare.java
@@ -1,12 +1,23 @@
 package io.github.yas99en.clipshare;
 
+import java.awt.AWTException;
+import java.io.IOException;
+
 import javax.swing.SwingUtilities;
+
+import io.github.yas99en.clipshare.view.Tray;
 
 public class ClipShare {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            System.out.println("run");
-        });
+        SwingUtilities.invokeLater(ClipShare::run);
+    }
+
+    public static void run() {
+        try {
+            new Tray();
+        } catch (IOException | AWTException e) {
+            e.printStackTrace();
+        }
     }
 }
--- clipshare/src/main/java/io/github/yas99en/clipshare/view/Tray.java
+++ clipshare/src/main/java/io/github/yas99en/clipshare/view/Tray.java
@@ -1,5 +1,20 @@
 package io.github.yas99en.clipshare.view;
 
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Tray {
+    public Tray() throws IOException, AWTException {
+        SystemTray tray = SystemTray.getSystemTray();
+        Image image = ImageIO.read(Tray.class.getResourceAsStream("clipboard_edit24.png"));
+        TrayIcon icon = new TrayIcon(image, "ClipShare");
+        tray.add(icon);
+    }
+
 
 }
